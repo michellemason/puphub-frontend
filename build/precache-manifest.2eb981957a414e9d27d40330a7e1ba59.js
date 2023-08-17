5ecf972d8b08453449132b67cd1d26f45d1cc232
Michelle Mason <michellemason95@yahoo.com>
2023-08-17 17:57:55 +0100

    updated README

---
+++ build/precache-manifest.2eb981957a414e9d27d40330a7e1ba59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4169ba04ea9c9b0c1558",
+    "url": "/static/css/main.ffcced55.chunk.css"
+  },
+  {
+    "revision": "4169ba04ea9c9b0c1558",
+    "url": "/static/js/main.4169ba04.chunk.js"
+  },
+  {
+    "revision": "82e7de66697f0217dfca",
+    "url": "/static/css/1.004f713b.chunk.css"
+  },
+  {
+    "revision": "82e7de66697f0217dfca",
+    "url": "/static/js/1.82e7de66.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "442155179122e6c46e215a686ae9fa5c",
+    "url": "/index.html"
+  }
+];
